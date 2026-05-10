f6a5f3fbaa0ee4f9922e9a6a6f7bf2430b25cc74
Tech57625 <tech57625@gmail.com>
2026-05-11 01:32:28 +0530

    output 20

---
+++ max.js
@@ -0,0 +1,15 @@
+// Input [10, 5, 20, 8]
+
+// output = 20
+
+let arr = [10, 5, 20, 8]
+
+let max = arr[0];
+
+for(let i =1; i<arr.length; i++){
+    if(arr[i]>max){
+        max = arr[i]
+    }
+}
+
+console.log(max);
